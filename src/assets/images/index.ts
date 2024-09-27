dafe322e78661dd0c0bec61955e223b00dc8128c
nau <lephamxuanduyen@gmail.com>
2024-09-27 10:29:35 +0700

    feat: homeScreen

---
+++ src/assets/images/index.ts
@@ -0,0 +1,9 @@
+import homeBg from './home-background.png'
+import logo from './logo.png'
+import startBtn from './tap-to-play.png'
+
+export {
+    homeBg,
+    logo,
+    startBtn
+}
--- src/presentation/pages/Home.tsx
+++ src/presentation/pages/Home.tsx
@@ -1,16 +1,22 @@
 import { useNavigation } from '@react-navigation/native'
 import React from 'react'
-import { Text, StyleSheet } from 'react-native'
+import { StyleSheet, View, Image, TouchableOpacity } from 'react-native'
+import { homeBg, logo, startBtn } from '../../assets/images'
 
 const Home = () => {
     const navigation = useNavigation()
     return (
-        <Text
-            style={styles.container}
-            onPress={() => {
-                navigation.replace('gamePlay')
-            }}
-        >Home Screen</Text>
+        <View>
+            <Image source={homeBg}
+                style={styles.container} />
+            <TouchableOpacity
+                style={styles.startGame}
+                onPress={() => navigation.replace('gamePlay')}
+            >
+                <Image source={logo} />
+                <Image source={startBtn} />
+            </TouchableOpacity>
+        </View>
     )
 }
 
@@ -18,8 +24,18 @@ const styles = StyleSheet.create({
     container: {
         justifyContent: 'center',
         alignContent: 'center',
-        fontSize: 40
-    }
+        position: 'relative',
+        width: '100%',
+        height: '100%'
+    },
+    startGame: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        gap: 40,
+    },
 }
 )
 
